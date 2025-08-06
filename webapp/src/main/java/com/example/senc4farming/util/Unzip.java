413393b2977eba998bf6c6b526ebcedd327a829b
eneascarneiro <79973955+eneascarneiro@users.noreply.github.com>
2025-08-06 18:52:46 +0200

    update

--- webapp/src/main/java/com/example/senc4farming/util/Unzip.java
+++ webapp/src/main/java/com/example/senc4farming/util/Unzip.java
@@ -1,5 +1,7 @@
 package com.example.senc4farming.util;
 
+import org.apache.tomcat.util.http.fileupload.FileUtils;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
@@ -8,7 +10,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
-import org.apache.commons.io.FileUtils;
+
 
 /**
  * Slightly modified version of https://www.mkyong.com/java/how-to-decompress-files-from-a-zip-file/
@@ -17,7 +19,7 @@ import org.apache.commons.io.FileUtils;
 public class Unzip {
 
     private final File input;
-    private List<File> output = new ArrayList<>();
+    private final List<File> output = new ArrayList<>();
     private String outputFolder = "C:\\KMZ_TEMP";
 
     public Unzip(File file) {
@@ -44,11 +46,8 @@ public class Unzip {
 
     /**
      * Method to unzip the input file.
-     * <p>
-     * @param zipFile input zip file
-     * @throws java.lang.Exception
      */
-    public void unZipIt() throws Exception, IOException {
+    public void unZipIt() throws IOException {
 
         byte[] buffer = new byte[1024];
 
